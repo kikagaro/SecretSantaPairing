9b4c1e2ecd9ce81e50d0d3cc629ca5858793d0b5
kikagaro <kikagaro@gmail.com>
2021-07-14 18:51:52 -0400

    updated code with ability to create output.txt for record keeping of matches

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import random
 
 santas = []
 gifted = []
+lst = []
 count = 1
 
-with open('list.txt', 'r') as s:
-    for name in s:
-        santas.append(name.strip())
+try:
+    with open('list.txt', 'r') as s:
+        for name in s:
+            santas.append(name.strip())
+except FileNotFoundError:
+    print('list.txt does not exist.\n exiting...')
+    exit()
 
 gifted = santas[:]
 
@@ -16,6 +21,15 @@ for x in santas:
     gift = random.choice(gifted)
     while gift is x:
         gift = random.choice(gifted)
-    print(str(count) + ': ' + x + ' will draw for ' + gift)
+    message = str(count) + ': ' + x + ' will draw for ' + gift
+    print(message)
+    lst.append(message)
     count += 1
     gifted.remove(gift)
+
+try:
+    with open('output.txt', 'a') as o:
+        for c in lst:
+            o.write(c + '\n')
+except:
+    print('something went wrong')
