a1848bf05e1c3b56040260fb26b5c4898efc495d
Philip Wortinger <philip.wortinger@rightbrainnetworks.com>
2021-11-17 17:40:02 -0500

    da fuck kevin

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 
 import random
 
 santas = []
-gifted = []
-lst = []
-count = 1
 
 try:
     with open('list.txt', 'r') as s:
@@ -15,21 +12,39 @@ except FileNotFoundError:
     print('list.txt does not exist.\n exiting...')
     exit()
 
-gifted = santas[:]
 
-for x in santas:
-    gift = random.choice(gifted)
-    while gift is x:
+def retry(func):
+    def inner(*args, **kwargs):
+        while True:
+            try:
+                return func(*args, **kwargs)
+            except:
+                pass
+    return inner
+
+
+@retry
+def generate_list():
+    gifted = santas[:]
+    lst = []
+    for x in santas:
         gift = random.choice(gifted)
-    message = str(count) + ': ' + x + ' will draw for ' + gift
-    print(message)
-    lst.append(message)
-    count += 1
-    gifted.remove(gift)
+        if gift is x and len(gifted) == 1:
+            raise RuntimeError('Single person left in list, regenerating.')
+        while gift is x:
+            gift = random.choice(gifted)
+        message = f'{len(lst) + 1}: {x} will draw for {gift}'
+        lst.append(message)
+        gifted.remove(gift)
+    return lst
+
+
+result = generate_list()
+list(map(lambda x: print(x), result))
 
 try:
     with open('output.txt', 'a') as o:
-        for c in lst:
+        for c in result:
             o.write(c + '\n')
 except:
     print('something went wrong')
